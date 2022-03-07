ef401be4151c75a1be707ba6b986c252b67b7cc8
HildaPech <72896837+HildaPech@users.noreply.github.com>
2022-03-06 23:19:11 -0500

    SearchView

--- app/src/main/java/com/example/ejemplotoolbar/MainActivity.kt
+++ app/src/main/java/com/example/ejemplotoolbar/MainActivity.kt
@@ -3,19 +3,19 @@ package com.example.ejemplotoolbar
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
 import android.widget.Button
 import android.widget.Toast
+import androidx.appcompat.widget.SearchView
 import androidx.appcompat.widget.Toolbar
 
 class MainActivity : AppCompatActivity() {
-
     var toolbar: Toolbar? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
         toolbar = findViewById(R.id.toolbar)
         toolbar?.setTitle(R.string.app_name)
         setSupportActionBar(toolbar)
@@ -29,9 +29,33 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         menuInflater.inflate(R.menu.menu, menu)
+
+        val itemBusqueda = menu?.findItem(R.id.busqueda)
+        var vistaBusqueda = itemBusqueda?.actionView as SearchView
+
+        vistaBusqueda.queryHint = "Escribe tu busqueda..."
+
+        vistaBusqueda.setOnQueryTextFocusChangeListener { v, hasFocus ->
+            Log.d("LISTENERFOCUS", hasFocus.toString())
+        }
+        vistaBusqueda.setOnQueryTextListener(object: SearchView.OnQueryTextListener{
+            //Cuando el usuario Escribe
+            override fun onQueryTextChange(newText: String?): Boolean {
+                Log.d("OnQueryTextChange", newText.toString())
+                return true
+            }
+
+            //Cuando el usuario de enter
+
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                Log.d("OnQueryTextSubmit", query.toString())
+                return true
+            }
+
+        })
+
         return super.onCreateOptionsMenu(menu)
     }
-
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         when(item?.itemId){
             R.id.bFav -> {
@@ -39,7 +63,6 @@ class MainActivity : AppCompatActivity() {
                 return true
             }
             else->{return super.onOptionsItemSelected(item)}
-
         }
     }
 }
