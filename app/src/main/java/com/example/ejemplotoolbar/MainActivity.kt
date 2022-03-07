430f96079954d4f1925fb0c243e7e742739b6948
HildaPech <72896837+HildaPech@users.noreply.github.com>
2022-03-07 00:37:11 -0500

    Compartir contenido

--- app/src/main/java/com/example/ejemplotoolbar/MainActivity.kt
+++ app/src/main/java/com/example/ejemplotoolbar/MainActivity.kt
@@ -9,7 +9,9 @@ import android.view.MenuItem
 import android.widget.Button
 import android.widget.Toast
 import androidx.appcompat.widget.SearchView
+import androidx.appcompat.widget.ShareActionProvider
 import androidx.appcompat.widget.Toolbar
+import androidx.core.view.MenuItemCompat
 
 class MainActivity : AppCompatActivity() {
     var toolbar: Toolbar? = null
@@ -32,6 +34,12 @@ class MainActivity : AppCompatActivity() {
 
         val itemBusqueda = menu?.findItem(R.id.busqueda)
         var vistaBusqueda = itemBusqueda?.actionView as SearchView
+
+        val itemCompartir = menu?.findItem(R.id.share)
+        val shareActionProvider = MenuItemCompat.getActionProvider(itemCompartir) as ShareActionProvider
+        compartirIntent(shareActionProvider)
+
+
 
         vistaBusqueda.queryHint = "Escribe tu busqueda..."
 
@@ -63,6 +71,14 @@ class MainActivity : AppCompatActivity() {
                 return true
             }
             else->{return super.onOptionsItemSelected(item)}
+        }
+    }
+    private fun compartirIntent(shareActionProvider: ShareActionProvider){
+        if(shareActionProvider != null){
+            val intent = Intent(Intent.ACTION_SEND)
+            intent.type = "text/plain"
+            intent.putExtra(Intent.EXTRA_TEXT, "Este es un mensaje compartido - Hilda Yaroslavi")
+            shareActionProvider.setShareIntent(intent)
         }
     }
 }
