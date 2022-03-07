ac72e7ad185de1840d1755642d8333f0401b0d30
HildaPech <72896837+HildaPech@users.noreply.github.com>
2022-03-06 19:12:40 -0500

    Definir eventos a elementos

--- app/src/main/java/com/example/ejemplotoolbar/MainActivity.kt
+++ app/src/main/java/com/example/ejemplotoolbar/MainActivity.kt
@@ -2,6 +2,9 @@ package com.example.ejemplotoolbar
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
 import androidx.appcompat.widget.Toolbar
 
 class MainActivity : AppCompatActivity() {
@@ -14,5 +17,21 @@ class MainActivity : AppCompatActivity() {
         toolbar = findViewById(R.id.toolbar)
         toolbar?.setTitle(R.string.app_name)
         setSupportActionBar(toolbar)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item?.itemId){
+            R.id.bFav -> {
+                Toast.makeText(this, "Elemento añadido como favorito", Toast.LENGTH_SHORT).show()
+                return true
+            }
+            else->{return super.onOptionsItemSelected(item)}
+
+        }
     }
 }
